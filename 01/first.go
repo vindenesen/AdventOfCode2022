7d9611b4f37b2167a02eb792430cd05745cc2665
Trond Vindenes <vindenesen@gmail.com>
2022-12-03 11:19:04 +0100

    Added README.md and comments to code

--- 01/first.go
+++ 01/first.go
@@ -17,10 +17,15 @@ const listFile = "input"
 func main() {
 	fmt.Println("Day 1: Calorie Counting")
 
+	// Make a slice containing our elf's
 	elfs := make([]*elf.Elf, 0)
 
+	// Open input data and read it
 	file, err := os.Open(listFile)
-	defer file.Close()
+	defer func() {
+		err := file.Close()
+		fmt.Println(err)
+	}()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,6 +36,7 @@ func main() {
 	}
 	content := string(contentBytes)
 
+	// Looping through the input data, and assigning calories to elf's
 	elfCalories := strings.Split(content, "\n\n")
 	for elfId := 0; elfId < len(elfCalories); elfId++ {
 		newElf := elf.New(elfId)
@@ -47,6 +53,9 @@ func main() {
 		}
 	}
 
-	sort.Slice(elfs, func(i, j int) bool { return elfs[i].GetTotalCalories() < elfs[j].GetTotalCalories() })
-	fmt.Println(elfs[len(elfs)-1].GetTotalCalories())
+	// Sort list of elf's from the largest calorie count to the smallest
+	sort.Slice(elfs, func(i, j int) bool { return elfs[i].GetTotalCalories() > elfs[j].GetTotalCalories() })
+
+	// Print the elf with the most calories
+	fmt.Println(elfs[0].GetTotalCalories())
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+# AdventofCode2022
+My attempt at solving the Advent of Code 2022 by using Go
+
+I may not always attempt the easiest solution, since I'm also trying to advance my knowledge of the programming language Go.
